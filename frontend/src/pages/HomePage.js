a6fef60dc6039e39dfb2fc653c1f4dbdc0794bc0
sachin <sachinpatel.19.cs@iite.indusuni.ac.in>
2023-02-11 18:07:33 +0530

    redux-store-add

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,44 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Row } from 'react-bootstrap';
-import Product from '../components/Product';
-import axios from 'axios';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect, useState } from "react";
+import { Col, Row, TabContent } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { listProducts } from "../actions/productActions";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 function HomePage() {
-  const [products, setProducts] = useState([]);
-
+  const dispatch = useDispatch();
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   useEffect(() => {
-    // MAKE A REQUEST TO BACKEND
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-
-      setProducts(data);
-    };
-    fetchProducts();
+    dispatch(listProducts());
 
     // AOS Initialization
     Aos.init({
       duration: 1000,
     });
-  }, []);
+  }, [dispatch]);
+
   return (
     <>
-      <h1 className='text-center' data-aos='fade-up'>
+      
+      {loading ? (
+        
+        <Loader/>
+      
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <>
+        <h1 className="text-center" data-aos="fade-up">
         Latest Products
       </h1>
-      <Row>
-        {products.map((product) => {
-          return (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-              <Product product={product} />
-            </Col>
-          );
-        })}
-      </Row>
-      <h1>hii</h1>
+        <Row>
+          {products.map((product) => {
+            return (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            );
+          })}
+        </Row>
+        </>
+      )}
     </>
-   
   );
 }
 
